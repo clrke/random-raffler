eafec7fed5ca3851ca2444eb2d7f39b6ab5920a9
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-12-13 20:37:37 +0800

    Catch some exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("entries.txt")));
+    public static void main(String[] args) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(new File("entries.txt")));
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Error: entries.txt does not exist!");
+        }
         List<String> entries = new ArrayList<String>();
 
         String line;
-        while ((line = reader.readLine()) != null) {
-            if (!line.equals("")) {
-                entries.add(line);
+        try {
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals("")) {
+                    entries.add(line);
+                }
             }
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Something went wrong with reading the " +
+                    "entries file. Please try again.");
         }
 
         RandomRafflerFrame randomRafflerFrame = new RandomRafflerFrame(entries);
